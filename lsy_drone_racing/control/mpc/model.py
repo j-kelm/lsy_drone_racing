c3f8cc50e4f76c6ab78b2cc9e180711de01cc590
Jonathan Kelm <jonathan.kelm@rwth-aachen.de>
2024-12-29 15:41:51 +0100

    Small cleanup of mpc model

--- lsy_drone_racing/control/mpc/model.py
+++ lsy_drone_racing/control/mpc/model.py
@@ -47,8 +47,8 @@ class DeltaModel:
         g, length = self.g, self.L
 
         J = cs.blockcat([[self.Ixx, 0.0, 0.0],
-                         [0.0, self.Iyy, 0.0],
-                         [0.0, 0.0, self.Izz]])
+                        [0.0, self.Iyy, 0.0],
+                        [0.0, 0.0, self.Izz]])
         Jinv = cs.blockcat([[1.0 / self.Ixx, 0.0, 0.0],
                             [0.0, 1.0 / self.Iyy, 0.0],
                             [0.0, 0.0, 1.0 / self.Izz]])
@@ -59,26 +59,32 @@ class DeltaModel:
         x = cs.MX.sym('x')
         y = cs.MX.sym('y')
         z = cs.MX.sym('z')
+        pos = cs.vertcat(x, y, z)
+
         x_dot = cs.MX.sym('x_dot')
         y_dot = cs.MX.sym('y_dot')
         z_dot = cs.MX.sym('z_dot')
+        pos_dot = cs.vertcat(x_dot, y_dot, z_dot)
+
         phi = cs.MX.sym('phi')  # Roll
         theta = cs.MX.sym('theta')  # Pitch
         psi = cs.MX.sym('psi')  # Yaw
+        ang = cs.vertcat(phi, theta, psi)
+
         p_body = cs.MX.sym('p')  # Body frame roll rate
         q_body = cs.MX.sym('q')  # body frame pitch rate
         r_body = cs.MX.sym('r')  # body frame yaw rate
+        rate = cs.vertcat(p_body, q_body, r_body)
 
         # Input states
         f1 = cs.MX.sym('f1')
         f2 = cs.MX.sym('f2')
         f3 = cs.MX.sym('f3')
         f4 = cs.MX.sym('f4')
-        # PyBullet Euler angles use the SDFormat for rotation matrices.
-        Rob = csRotXYZ(phi, theta, psi)  # rotation matrix transforming a vector in the body frame to the world frame.
+        thrusts = cs.vertcat(f1, f2, f3, f4)
 
         # Define state variables.
-        X = cs.vertcat(x, y, z, x_dot, y_dot, z_dot, phi, theta, psi, p_body, q_body, r_body, f1, f2, f3, f4)
+        X = cs.vertcat(pos, pos_dot, ang, rate, thrusts)
 
         # Define inputs.
         f1_dot = cs.MX.sym('df1')
@@ -93,19 +99,20 @@ class DeltaModel:
         # Defining the dynamics function.
         # We are using the velocity of the base wrt to the world frame expressed in the world frame.
         # Note that the reference expresses this in the body frame.
+        # PyBullet Euler angles use the SDFormat for rotation matrices.
+        Rob = csRotXYZ(phi, theta, psi)  # rotation matrix transforming a vector in the body frame to the world frame.
         oVdot_cg_o = Rob @ cs.vertcat(0, 0, f1 + f2 + f3 + f4) / self.m - cs.vertcat(0, 0, g)
         pos_ddot = oVdot_cg_o
-        pos_dot = cs.vertcat(x_dot, y_dot, z_dot)
         Mb = cs.vertcat(length / cs.sqrt(2.0) * (f1 + f2 - f3 - f4),
                         length / cs.sqrt(2.0) * (-f1 + f2 + f3 - f4),
                         gamma * (f1 - f2 + f3 - f4))
-        rate_dot = Jinv @ (Mb - (cs.skew(cs.vertcat(p_body, q_body, r_body)) @ J @ cs.vertcat(p_body, q_body, r_body)))
+        rate_dot = Jinv @ (Mb - (cs.skew(rate) @ J @ rate))
         ang_dot = cs.blockcat([[1, cs.sin(phi) * cs.tan(theta), cs.cos(phi) * cs.tan(theta)],
                                 [0, cs.cos(phi), -cs.sin(phi)],
-                                [0, cs.sin(phi) / cs.cos(theta), cs.cos(phi) / cs.cos(theta)]]) @ cs.vertcat(p_body, q_body, r_body)
+                                [0, cs.sin(phi) / cs.cos(theta), cs.cos(phi) / cs.cos(theta)]]) @ rate
         X_dot = cs.vertcat(pos_dot, pos_ddot, ang_dot, rate_dot, U)
 
-        Y = cs.vertcat(x, y, z, x_dot, y_dot, z_dot, pos_ddot, phi, theta, psi, p_body, q_body, r_body, ang_dot, f1, f2, f3, f4)
+        Y = cs.vertcat(pos, pos_dot, pos_ddot, ang, rate, ang_dot, thrusts)
         # Set the equilibrium values for linearizations.
 
         self.X_EQ = np.zeros(nx)
